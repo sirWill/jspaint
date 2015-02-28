6adf5cff36893e7dfb23faf045619afdddf4ee20
Roman Spiridonov <re@spiridonovy.ru>
2015-02-28 22:34:42 +0300

    Added toolbar and started color picker

--- js/paint.js
+++ js/paint.js
@@ -9,7 +9,12 @@ var Paint = function(){
   ctx.fillRect(0,0,element.width, element.height);
   var line = false;
 
+  var colors = ['#03A9F4', '#F44336', '#CDDC39', '#9C27B0'];
+
   this.init = function init(){
+    
+    initToolbar();
+
     //рисуем линии
     element.addEventListener('click', function(e){
       e.preventDefault();
@@ -25,7 +30,13 @@ var Paint = function(){
       line = !line;
       ctx.fillRect(x-1,y-1,2,2);
     });
-  };
+
+  };/* init */
+
+
+  function initToolbar(){
+    
+  }
 
 }
 
