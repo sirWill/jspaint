3f298c13d0fd13c3cf2da0ecc59a9e5abdef5f75
Roman Spiridonov <re@spiridonovy.ru>
2015-03-01 02:14:53 +0300

    Added line preview when mousemove

--- js/paint.js
+++ js/paint.js
@@ -5,15 +5,42 @@ var Paint = function(){
   var ctx = element.getContext('2d');
   element.width = 800;
   element.height = 600;
+
   ctx.fillStyle = 'rgb(255,255,255)';
   ctx.fillRect(0,0,element.width, element.height);
-  var line = false;
+  ctx.fillStyle = '#000';
 
+  var line = false;
+  var buffer = null;
+  var buf = {x:null,y:null, h:null, w: null};
   var colors = document.querySelectorAll('input[name="color"]');
   var activeColor = 0;
+
+  var startPoint = {x:null, y:null};
+  var clean = ctx.getImageData(0,0,element.width,element.height);
+  
+  function save2Buffer(x0,y0,x1,y1){
+    if(x0>x1){
+      x0 = x0 + x1; x1 = x0 - x1; x0 = x0 - x1;
+    }
+    if(y0>y1){
+      y0 = y0 + y1; y1 = y0 - y1; y0 = y0 - y1;
+    }
+    x0-=5; y0-=5;
+    x1+=5; y1+=5;
+    buf.x = x0; buf.y = y0;
+    buf.w = x1-x0; buf.h = y1-y0;
+    buffer = ctx.getImageData(buf.x,buf.y,buf.w,buf.h);
+  } /* save2Buffer */
+
+  function restoreBuffer(){
+    ctx.putImageData(buffer,buf.x,buf.y);
+    buffer = null;
+  } /* restoreBuffer */
+
   this.init = function init(){
-    
     initToolbar();
+    setActiveColor(0);
 
     //рисуем линии
     element.addEventListener('click', function(e){
@@ -23,13 +50,35 @@ var Paint = function(){
       if(line){
         ctx.lineTo(x,y);
         ctx.stroke();
+        buffer = null;
       }else{
+        startPoint.x = x;
+        startPoint.y = y;
         ctx.beginPath();
         ctx.moveTo(x,y);
       }
       line = !line;
-      ctx.fillRect(x-1,y-1,2,2);
+      // ctx.fillRect(x-1,y-1,2,2);
     });
+
+    element.addEventListener('dblclick', function(e){
+      ctx.putImageData(clean,0,0);
+    });
+    
+    element.addEventListener('mousemove', function(e){
+      e.preventDefault();
+      if(!line) return;
+      if(buffer){
+        restoreBuffer();
+      }
+      save2Buffer(startPoint.x, startPoint.y, e.offsetX, e.offsetY);
+
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(startPoint.x,startPoint.y);
+    });
+
 
   };/* init */
 
@@ -48,8 +97,7 @@ var Paint = function(){
     for(var i=0; i<colors.length; i++){
       colors[i].addEventListener('change', colorSelect);
     }
-    // document.querySelectorAll('input[name="color"]:checked+span')
-  }
+  } /* initToolbar */
 
 }
 
